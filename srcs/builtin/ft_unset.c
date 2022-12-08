6521af966281c618bcfd7ff6b32c0edab7a82e01
seokunee <xocoolree@gmail.com>
2022-12-08 17:13:41 +0900

    unset 임시저장

--- srcs/builtin/ft_unset.c
+++ srcs/builtin/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 23:33:26 by seokchoi          #+#    #+#             */
-/*   Updated: 2022/12/07 23:38:09 by seokchoi         ###   ########.fr       */
+/*   Updated: 2022/12/08 16:14:46 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,11 @@
 void	ft_unset(t_token *token)
 {
 	
+	if (token->cmd[1])
+	{
+		// ft_unset_env_list(token->info->env_list, cm)
+		errno = 0;
+	}
+	else
+		return ;
 }
